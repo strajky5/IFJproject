f3b43aba602cefd8e02640202a52ff95a62b9643
strajky5 <xhynek09@stud.fit.vutbr.cz>
2014-12-11 15:11:25 +0100

    2.3.8

--- interpret.h
+++ interpret.h
@@ -1,42 +1,27 @@
 #include "list.h"
 #include "str.h"
-#include "interpret.c"
 #include "ial.h"
-#include "scaner.h"
-#include <math.h>
+#include "errors.h"
 
 typedef struct Stack TStack;
 typedef struct StackItem TStackItem;
-struct StackItem{   			// struktura pro prvek zasobnika
-								// obsahuje:
-tParamItem *op1;					// ukazatel na operand
-TStack * pdown;					// ukazatel na prvek pod nim
+TStack stack;  //Globálna premenná  pre zásobník
 
+struct TStack{   				// struktura zasobnika
+	TStackItem *top;				// ukazatel na vrchol zasobniku
+};
+
+struct StackItem{   			// struktura pro prvek zasobnika
+	tParamItem *op1;					// ukazatel na operand
+	TStack *pdown;					// ukazatel na prvek pod nim
 };
 
 void stackinit(TStack *stack);								//funkce na inicializaci zasobniku
 tErrors stackPush(TStack *stack,tParamItem *op);			// pro pushnuti na zasobnik
 tParamItem* TStackTopPop(TStack *stack);					// pro vziti ze zasobniku
 void StackDeleteDataDelete(TStack *stack);					// pro uvolneni zasobniku z pameti
-quickSort( int a[], int l, int r);							// pro serazeni prvku pole
-int partition( int a[], int l, int r);						// pomovna funkce pro quicksort
 tParamItem *SearchStackName(string*Search);
 string *conc(string*s1,string*s2);
-tErrors interpret()
+tErrors interpret();
 
-
-TStack stack;  //Globálna premenná  pre zásobník
-
-struct TStackItem{   			// struktura pro prvek zasobnika
-								// obsahuje:
-tParamItem *op1;					// ukazatel na operand
-TStack * pdown;					// ukazatel na prvek pod nim
-
-};
-
-struct TStack{   				// struktura zasobnika
-
-TStackItem * top;				// ukazatel na vrchol zasobniku
-
-};
 
